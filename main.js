49d58f527f781272fd3f3bcce30afcaaf73aa3b5
Mohamed Mohamed <mgm6597@gmail.com>
2021-04-26 23:50:02 -0400

    Program now accepts arguments from command line and updated convertToDigits function to output the phonetic equivalent of the integer.

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-console.log(convertToDigits(67824));
+
+// Dictionary of values.
+var dict = [{key: 0, value: 'Zero'},
+            {key: 1, value: 'One'},
+            {key: 2, value: 'Two'},
+            {key: 3, value: 'Three'},
+            {key: 4, value: 'Four'},
+            {key: 5, value: 'Five'},
+            {key: 6, value: 'Six'},
+            {key: 7, value: 'Seven'},
+            {key: 8, value: 'Eight'},
+            {key: 9, value: 'Nine'}];
+
+// Store arguments into variable
+var args = process.argv.slice(2);
+
+
+// Mapping function to loop through each integer input.
+let array = args.map(function(item, index, array){
+  return convertToDigits(Number(item));
+});
+
+// Test Output
+console.log(array);
+
 // Function that takes in Integer and returns each digit in an array.
+// UPDATE: Function now returns the phonetic equivalent of the Integer ex: 97 --> ['Nine', 'Seven'].
 function convertToDigits(num) {
   var array = [];
   while(num >= 10){
-    array.unshift(num % 10);
+    array.unshift(dict[num % 10].value);
     num = Math.floor(num / 10);
   }
-  array.unshift(num);
+  array.unshift(dict[num].value);
   return array;
 }
